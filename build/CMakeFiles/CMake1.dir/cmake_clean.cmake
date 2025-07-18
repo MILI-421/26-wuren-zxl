0395ae48f642ccd5454f773d75984d2cafb8d4ef
zxl <3957321954@qq.com>
2025-07-18 23:28:39 +0800

    7月16日c++作业

---
+++ build/CMakeFiles/CMake1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMake1"
+  "CMake1.pdb"
+  "CMakeFiles/CMake1.dir/main.cpp.o"
+  "CMakeFiles/CMake1.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMake1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
